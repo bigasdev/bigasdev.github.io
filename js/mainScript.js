1957c57e48f40d9162b6ec8186e1233b2414bfea
gustavo domingues <deathdiec@gmail.com>
2022-05-25 15:55:15 -0300

    polishing the styles

---
+++ js/mainScript.js
@@ -0,0 +1,5 @@
+
+document.getElementById("twitterButton").addEventListener("click", () => {window.open("https://twitter.com/bigasdev");})
+document.getElementById("githubButton").addEventListener("click", () => {
+        window.open("https://github.com/bigasdev");
+})
