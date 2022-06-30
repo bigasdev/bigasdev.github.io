dd4a0ac512b2968da0906a0d07d5e116b365d3c2
gustavo domingues <deathdiec@gmail.com>
2022-06-30 10:29:24 -0300

    test webpack

---
+++ js/dist/main.js
@@ -0,0 +1,1 @@
+document.getElementById("twitterButton").addEventListener("click",(()=>{window.open("https://twitter.com/bigasdev")})),document.getElementById("githubButton").addEventListener("click",(()=>{window.open("https://github.com/bigasdev")}));
