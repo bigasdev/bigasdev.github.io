1aecea4974ed16be4b96f2536da7bc6f58618519
gustavo domingues <deathdiec@gmail.com>
2022-06-30 10:49:52 -0300

    Revert "test with node"
    This reverts commit 12c31172c5c180c604f28b33257d79b4b78f96ac.

--- js/articles.js
+++
@@ -1,9 +0,0 @@
-const { Client } = require('@notionhq/client');
-
-const notion = new Client({ auth: process.env.NOTION_API_KEY });
-
-(async () => {
-  const pageId = 'b55c9c91-384d-452b-81db-d1ef79372b75';
-  const response = await notion.pages.retrieve({ page_id: pageId });
-  console.log(response);
-})();
