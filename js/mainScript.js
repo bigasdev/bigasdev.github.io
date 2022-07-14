773cc030b8d587beba0016d9985bf39856a46314
gustavo domingues <deathdiec@gmail.com>
2022-07-13 22:40:35 -0300

    toc script + styling for mobile test again

--- js/mainScript.js
+++ js/mainScript.js
@@ -3,4 +3,19 @@ document.getElementById("twitterButton").addEventListener("click", () => {window
 document.getElementById("githubButton").addEventListener("click", () => {
         window.open("https://github.com/bigasdev");
 })
-  
+
+function checkForScroll(){
+    var toc = document.getElementById("toc")
+    if(toc != null){
+        document.addEventListener("scroll", ()=>{
+                var y = window.scrollY;
+                if(y >= 180){
+                        toc.style.display = "block";
+                }else{
+                        toc.style.display = "none";
+                }
+        })
+    }
+}
+
+checkForScroll();
